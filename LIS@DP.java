b77b5b3f940f0527b0013b29eba12ffbe2fffacb
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2021-03-07 01:39:50 +0530

    Add files via upload

---
+++ LIS@DP.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+        int dp[]=new int[n];
+        dp[0]=1;
+        for(int i=1;i<n;i++)
+        { dp[i]=1;
+            for(int j=i-1;j>=0;j--)
+            {
+                if(nums[j]<nums[i] && dp[j]+1>dp[i])
+                {
+                    dp[i]=dp[j]+1;
+                }
+            }
+        }
+        int max=0;
+        for(int i=0;i<n;i++)
+        {
+            max=Math.max(dp[i],max);
+        }
+        return max;
+    }
+}
