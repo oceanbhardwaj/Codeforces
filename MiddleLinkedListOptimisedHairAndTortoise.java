3ea0192b664c88b43132c66349304a3678808dfb
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2021-12-29 00:39:50 +0530

    Add files via upload

---
+++ MiddleLinkedListOptimisedHairAndTortoise.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int count=0;
+        ListNode curr=head;
+        while(curr!=null)
+        {
+            count++;
+            curr=curr.next;
+        }
+        for(int i=0;i<=count/2;i++)
+        {
+       curr=head;
+            head=head.next;
+        
+        }
+        return curr;
+        
+    }
+}
+///////////////////////////////////////////
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head==null || head.next==null )
+            return head;
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
